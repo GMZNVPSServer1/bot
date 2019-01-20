5f800ea749400d38c6b119daec61f12b29e4fc0c
GMZNVPSServer1 <46727528+GMZNVPSServer1@users.noreply.github.com>
2019-01-20 12:50:40 +0100

    Add files via upload

---
+++ bot.js.js
@@ -0,0 +1,59 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+console.log("Welcome Again !");
+});
+
+
+
+
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`
+**
+سلام عليكم يا قلبي ● Server :
+ → Carezma Server..
+
+ّ
+
+بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا حياك الله,
+
+Carezma Server..
+
+الرابط
+
+
+ [https://discord.gg/jW9GGnU]
+@everyone
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`
+**
+سلام عليكم يا قلبي ● Server :
+ → Carezma Server
+ّ
+
+بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا حياك الله,
+
+Carezma Ser
+
+
+الرابط
+
+
+[https://discord.gg/jW9GGnU]
+@everyone
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+})
+
+client.login("NTE0NTQ2NDc3MDQxOTc1Mjk2.Dxz9cA.frnnPKIK-FkXw_ehVau45-yTaJI");
